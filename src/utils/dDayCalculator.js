3f48724381cba6ffc3e67b5ed7a549a906183bb4
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-11-15 21:08:17 +0900

    chore: 린트 경고문에 따른 코드 수정

--- src/utils/dDayCalculator.js
+++ src/utils/dDayCalculator.js
@@ -3,7 +3,9 @@ import NUMBERS from '../constants/numbers/numbers';
 const dDayCalculator = day => {
   let discountAmount = NUMBERS.dDayDiscountStandardAmount;
   const dDay = day - 1;
+
   discountAmount += dDay * NUMBERS.dDayDiscountAmount;
+
   return discountAmount;
 };
 
